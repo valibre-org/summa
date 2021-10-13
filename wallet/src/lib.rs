79f8e3437fbdfe6d5aab0e55d6f30853060d3bc5
Daniel Olano <daniel@olanod.com>
2021-10-13 22:32:06 +0200

    Create MatrixVault skeleton

--- wallet/src/lib.rs
+++ wallet/src/lib.rs
@@ -1,7 +1,37 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+use libwallet::{async_trait, ed25519, CryptoType, Vault};
+
+pub use libwallet::Wallet;
+
+/// A `MatrixVault` stores in memory a private key extracted from the
+/// Secure Secret Storage and Sharing system of Matrix that comes in the form of
+/// a JSON object that looks like this:
+/// ```json
+/// {
+///   "encrypted": {
+///     "awesomeKeyId": {
+///       "iv": "supercooliv",
+///       "ciphertext": "secretciphertext",
+///       "mac": "macthingy"
+///     }
+///   }
+/// }
+/// ```
+pub struct MatrixVault;
+
+impl CryptoType for MatrixVault {
+    type Pair = ed25519::Pair;
+}
+
+impl MatrixVault {
+    pub fn new() -> Self {
+        MatrixVault
     }
 }
+
+#[async_trait(?Send)]
+impl Vault for MatrixVault {
+    async fn unlock(&self, _password: &str) -> libwallet::Result<Self::Pair> {
+        todo!()
+    }
+}
+
